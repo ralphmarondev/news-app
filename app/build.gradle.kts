578314e1d63d9954b5131351dec5d12fe9044e02
Ralph Maron Eda <edaralphmaron@gmail.com>
2024-11-01 08:10:12 +0800

    added dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,11 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+    implementation(libs.androidx.material.icons.extended.android)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.news.api.java)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
---
+++ app/src/main/java/com/ralphmarondev/newsapp/core/utils/Constant.kt
@@ -0,0 +1,6 @@
+package com.ralphmarondev.newsapp.core.utils
+
+object Constant {
+    // please don't use my api key for bad purposes in case you are reading this. beggg :<
+    const val API_KEY = "5ce142cd43c74e2ead0402bbb926f153"
+}
