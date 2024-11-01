fabef05121e91d13c55f08d9d7388b603d0cfd50
Ralph Maron Eda <edaralphmaron@gmail.com>
2024-11-01 08:00:45 +0800

    initial commit

---
+++ app/src/main/java/com/ralphmarondev/newsapp/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.ralphmarondev.newsapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.ralphmarondev.newsapp.ui.theme.NewsAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            NewsAppTheme {
+
+            }
+        }
+    }
+}
